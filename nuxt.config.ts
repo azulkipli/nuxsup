ebde22e3532ea8d0bbf78be00d9eddecc8856c94
azul <azulkipli@gmail.com>
2026-02-12 09:26:39 +0700

    generate new supabase nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
+  ssr: false,
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase'],
-  css: ['./app/assets/css/main.css'],
+  modules: ["@nuxtjs/supabase"],
+  css: ["./app/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss() as any,
-    ],
+    plugins: [tailwindcss() as any],
   },
   supabase: {
     types: false, // disable database types
     redirectOptions: {
-      login: '/login',
-      callback: '/confirm',
+      login: "/login",
+      callback: "/confirm",
       include: undefined,
-      exclude: ['/', '/register', '/about'],
+      exclude: ["/", "/register", "/about"],
       saveRedirectToCookie: true,
     },
   },
-})
+});
