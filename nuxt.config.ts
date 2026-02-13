15ef8862c3364cd9ae5985e67f5450f9b06d914f
azul <azulkipli@gmail.com>
2026-02-13 18:27:06 +0700

    feat: install @vite-pwa/nuxt module and web-push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   ssr: false,
   devtools: { enabled: true },
-  modules: ["@nuxtjs/supabase"],
+  modules: ["@nuxtjs/supabase", "@vite-pwa/nuxt"],
   css: ["./app/assets/css/main.css"],
   vite: {
     server: {
